3e668cf232e57fb8c9cbfb271f7b522fd70e0ed6
Clauvet1 <mongambac@gmail.com>
2024-04-01 21:58:55 +0100

    backEnd second commit

--- src/Signup.js
+++ src/Signup.js
@@ -6,11 +6,34 @@ const Signup = () => {
     const handleUserTypeChange = (type) => {
       setUserType(type);
     };
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
   
-    const handleSignup = () => {
-      // Add your signup logic here
-    //   console.log(Signing up as ${userType});
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const userData = { fullName, email, password };
+      console.log('User Data:', userData);
+  
+    
+      try {
+        const response = await fetch('http://localhost:3001/Signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(userData),
+        });
+        if (response.ok) {
+          console.log('User data sent successfully to the server');
+        } else {
+          console.error('Failed to send user data to the server');
+        }
+      } catch (error) {
+        console.error('Error sending user data:', error);
+      }
     };
+    
   
     return (
       <div className="login-container">
@@ -19,11 +42,11 @@ const Signup = () => {
           <button className={userType === 'mentor' ? 'active' : ''} onClick={() => handleUserTypeChange('mentor')}>Mentor</button>
           <button className={userType === 'mentee' ? 'active' : ''} onClick={() => handleUserTypeChange('mentee')}>Mentee</button>
         </div>
-        <form>
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button className=" m-2 px-5 rounded-5" onClick={handleSignup}>Signup as {userType}</button>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={fullName} onChange={e => setFullName(e.target.value)}  placeholder="Full Name" />
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+          <button className=" m-2 px-5 rounded-5" >Signup as {userType}</button>
         </form>
       </div>
     );
